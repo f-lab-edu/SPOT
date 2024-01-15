c262b5ca34c6b82e01711eeadd100e3942c78c37
MANSA10004 <mansa@corca.ai>
2024-01-15 12:12:51 +0900

    feat: 프로젝트 셋팅 interface: SPM 패키지 & 테스트 코드 속도 향상 셋팅

---
+++ SPOT/SPOT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPOT
+//
+//  Created by 만리자 on 1/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SPOT/SPOT/SPOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPOTApp.swift
+//  SPOT
+//
+//  Created by 만리자 on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SPOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
