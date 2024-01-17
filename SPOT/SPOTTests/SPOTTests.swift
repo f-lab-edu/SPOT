5636363dab2383d0c9a46b21153465edd8c23972
MANSA10004 <mansa@corca.ai>
2024-01-17 11:56:44 +0900

    chore: CI Test

--- SPOT/SPOTTests/SPOTTests.swift
+++ SPOT/SPOTTests/SPOTTests.swift
@@ -29,6 +29,7 @@ final class SPOTTests: XCTestCase {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
+            // CI Test
         }
     }
 
