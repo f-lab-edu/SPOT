08df3bab19ea7c95bd9f4f3d637489453d751794
MANSA10004 <mansa@corca.ai>
2024-02-01 18:23:09 +0900

    feat: Running Location Presentaion 은 TCA로 작성

--- SPOT/Presentation/Package.swift
+++ SPOT/Presentation/Package.swift
@@ -7,16 +7,19 @@ let package = Package(
     name: "Presentation",
     platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Presentation",
             targets: ["Presentation"]),
     ],
+    dependencies: [
+        .package(path: "../Proxy")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Presentation"),
+            name: "Presentation",
+            dependencies: [
+                .product(name: "ComposableArchitectureWrapper", package: "Proxy")
+            ]),
         .testTarget(
             name: "PresentationTests",
             dependencies: ["Presentation"]),
