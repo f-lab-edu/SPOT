f862d01d70beeca6cae395406535b3b8bd658b37
MANSA10004 <mansa@corca.ai>
2024-02-13 18:48:52 +0900

    feat: Activity 파일 추가

---
+++ SPOT/DataAccess/Sources/RunningDataAccess/ActivityService.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityService.swift
+//
+//
+//  Created by 김민식 on 2024/02/13.
+//
+
+import CoreMotion
+import Combine
+
+import Controller
+import Entity
+
+public final class ActivityService: ActivityController {
+    
+}
---
+++ SPOT/Domain/Sources/Controller/ActivityController.swift
@@ -0,0 +1,14 @@
+//
+//  ActivityController.swift
+//  
+//
+//  Created by 김민식 on 2024/02/13.
+//
+
+import Combine
+import Foundation
+
+import Entity
+
+public protocol ActivityController {
+}
