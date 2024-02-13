3012b3b8144fb065d8aefa4da6ce6c4563168bfe
ITzombietux <zombietux@naver.com>
2024-02-14 01:29:07 +0900

    feat: Activity DataAccess 추가

--- SPOT/DataAccess/Sources/RunningDataAccess/ActivityService.swift
+++ SPOT/DataAccess/Sources/RunningDataAccess/ActivityService.swift
@@ -12,5 +12,39 @@ import Controller
 import Entity
 
 public final class ActivityService: ActivityController {
+    public var authorizationStatus = PassthroughSubject<ActivityAuthorizationStatus, Never>()
+    public var activity = PassthroughSubject<Entity.Activity, Never>()
+    private let pedometer: CMPedometer
     
+    public init(pedometer: CMPedometer) {
+        self.pedometer = pedometer
+    }
+    
+    public func checkAuthoization() {
+        switch CMPedometer.authorizationStatus() {
+        case .authorized: 
+            authorizationStatus.send(.authorized)
+        case .denied: 
+            authorizationStatus.send(.denied)
+        case .notDetermined: 
+            authorizationStatus.send(.notDetermined)
+        case .restricted: 
+            authorizationStatus.send(.restricted)
+        @unknown default: break
+        }
+    }
+    
+    public func startUpdates(startedAt: Date) {
+        pedometer.startUpdates(from: startedAt) { data, error in
+            guard let data else { return }
+            
+            let updated = Activity(distance: data.distance?.doubleValue ?? 0, pace: data.currentPace?.doubleValue ?? 0)
+            
+            self.activity.send(updated)
+        }
+    }
+    
+    public func stopUpdates() {
+        pedometer.stopUpdates()
+    }
 }
---
+++ SPOT/Domain/Sources/Entity/ActivityAuthorizationStatus.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityAuthorizationStatus.swift
+//
+//
+//  Created by 10004 on 2/14/24.
+//
+
+import Foundation
+
+public enum ActivityAuthorizationStatus {
+    case restricted
+    case authorized
+    case notDetermined
+    case denied
+}
